a4683e43c835e2e29cccc04aac745d75ca83703f
Jorn Kalu <kiceej@gmail.com>
2024-07-24 14:09:12 +0100

    fixed error

--- database/db.py
+++ database/db.py
@@ -40,8 +40,10 @@ def get_session():
     session = SessionLocal()
     try:
         yield session
+        session.commit()
     except:
         session.rollback()
+        raise
     finally:
         session.close()
 
